54aeced17f397b33b4c125d4f9f363f49882ac9c
aolson28 <123501344+aolson28@users.noreply.github.com>
2024-01-01 15:24:08 -0500

    Update script.js

--- text-checker/script.js
+++ text-checker/script.js
@@ -1,3 +1,29 @@
 const textInputElement = document.getElementById("text-input");
 
 const checkBtnElement = document.getElementById("check-btn");
+
+const resultElement = document.getElementById("result");
+
+const palindromeReplace = /[^a-z0-9]/g;
+
+const checkText = (input)=> {
+  if(input === '') {
+    alert("Please input a value")
+  } else {
+    const submittedInput = input;
+    input.toLowerCase().replace(plaindromeReplace, '');
+    const result;
+    if (input === input.reverse()) {
+      result = "";
+    } else {
+      result = " not";
+    }
+    resultElement.innerHTML = `${submittedInput} is${result} a palindrome`;
+    resultElement.classList.remove("hide");
+    textInputElement.innerHTML ="";
+  }
+}
+
+checkBtnElement.addEventListener("click",() => {
+  checkText(textInputElement.value);
+});
