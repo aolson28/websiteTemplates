0edb1311595be074c57f0b64fc672150880b8ddf
aolson28 <123501344+aolson28@users.noreply.github.com>
2024-01-03 16:31:44 -0500

    Update script.js

--- test-http-post/script.js
+++ test-http-post/script.js
@@ -27,3 +27,46 @@ checkBtnElement.addEventListener("click",() => {
   checkText(textInputElement.value);
 });
 */
+
+checkBtnElement.addEventListener("click",() => {
+  fetch("https://api.machinemetrics.com/reports/production", {
+  method: "POST",
+  body: JSON.stringify({
+  "start": "2023-08-01T05:00:00Z",
+  "end": "2023-12-21T05:00:00Z",
+  "data": [
+    {
+      "metric": "allTime"
+    },
+    {
+      "metric": "partsGoal"
+    },
+    {
+      "metric": "totalParts"
+    },
+    {
+      "metric": "oee"
+    }
+  ],
+  "groupBy": [
+    {
+      "group": "jobOperation"
+    },
+    {
+      "group": "hour"
+    },
+    {
+      "group": "machine"
+    }
+  ],
+  "csv": false,
+  "flatten": true
+}),
+  headers: {
+    "Content-type": "application/json",
+    "Authorization": "Bearer token"
+  }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+});
