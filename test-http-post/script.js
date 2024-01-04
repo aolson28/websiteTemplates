e963f1f8555c5c3751753354c2e85ed60fadc984
aolson28 <123501344+aolson28@users.noreply.github.com>
2024-01-04 12:18:20 -0500

    Update script.js

--- test-http-post/script.js
+++ test-http-post/script.js
@@ -3,32 +3,8 @@ const textInputElement = document.getElementById("text-input");
 const checkBtnElement = document.getElementById("check-btn");
 
 const resultElement = document.getElementById("result");
-/*
-const palindromeReplace = /[^a-z0-9]/g;
 
-const checkText = (input)=> {
-  if(input === '') {
-    alert("Please input a value")
-  } else {
-    const submittedInput = input;
-    const cleanStr = input.toLowerCase().replace(palindromeReplace, "");
-    let result ="";
-    if (cleanStr === cleanStr.split('').reverse().join('')) {
-      result = "";
-    } else {
-      result = " not";
-    }
-    resultElement.innerHTML = `${submittedInput} is${result} a palindrome`;
-    resultElement.classList.remove("hide");
-  }
-}
-
-checkBtnElement.addEventListener("click",() => {
-  checkText(textInputElement.value);
-});
-*/
-
-checkBtnElement.addEventListener("click",() => {
+const machineMetricsAPIPost = () => {
   fetch("https://api.machinemetrics.com/reports/production", {
   method: "POST",
   body: JSON.stringify({
@@ -69,4 +45,6 @@ checkBtnElement.addEventListener("click",() => {
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
-});
+}
+
+checkBtnElement.addEventListener("click",machineMetricsAPIPost());
